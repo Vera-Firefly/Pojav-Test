6460e0f5593bac8602dc3b155663a5c5a8529beb
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2023-11-12 11:47:39 +0800

    Update

--- app_pojavlauncher/src/main/jni/ctxbridges/osm_bridge.c
+++ app_pojavlauncher/src/main/jni/ctxbridges/osm_bridge.c
@@ -13,6 +13,12 @@ static __thread osm_render_window_t* currentBundle;
 static char no_render_buffer[4];
 
 bool osm_init() {
+    switch (pojav_environ->config_renderer) {
+        case RENDERER_VIRGL:
+            dlsym_OSMesa();
+            dlsym_EGL();
+            break;
+    }
     dlsym_OSMesa();
     return true; // no more specific initialization required
 }
